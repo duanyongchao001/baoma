1b610bf6fdbcb3d137f6b318dc8af9d25022e3c9
duan.yongchao <962013370@qq.com>
2020-10-31 10:44:30 +0800

    baoma version 0.1 by duanyongchao

---
+++ src/main/java/com/xunhe/tech_man_sys/TechManSysApplication.java
@@ -0,0 +1,13 @@
+package com.xunhe.tech_man_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechManSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechManSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xunhe/tech_man_sys/TechManSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xunhe.tech_man_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechManSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
